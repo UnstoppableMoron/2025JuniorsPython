8fec5efec7cda8b338aae0132a72f9cfc543a58f
UnstoppableMoron <cte-owen.baker@sllboces.org>
2025-05-08 17:51:04 +0000

    more numbers

---
+++ evenOdd.py
@@ -0,0 +1,12 @@
+def check_even_odd():
+    # Get user input
+    num = int(input("Enter a whole number: "))
+
+    # Use an if/else statement to check if the number is even or odd
+    if num % 2 == 0: 
+        print("The number is even.")
+    else:
+        print("The number is odd.")
+
+# Call the function to run the program
+check_even_odd
